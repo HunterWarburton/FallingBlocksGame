273b7af46c629d347e5cb481ce6aabc801f5752d
HunterWarburton <warburtonhunter@gmail.com>
2022-12-29 00:37:35 -0700

    Update fallingblocks.js
    complete rewrite, again

--- fallingblocks.js
+++ fallingblocks.js
@@ -1,36 +1,52 @@
+let numRows = 6;
+let numColumns = 6;
+let id = 0;
+let colors = ["red", "blue", "green", "orange", "purple", "yellow"];
+let selectedBlock = null;
+
 function generateGrid(numRows, numColumns) {
 let grid = [];
+let id = 0;
 for (let i = 0; i < numRows; i++) {
 let row = [];
 for (let j = 0; j < numColumns; j++) {
-let color = Math.random() > 0.5 ? "primary" : "secondary";
-let block = {color: color};
-row.push(block);
+let color = colors[Math.floor(Math.random() * colors.length)];
+row.push({color: color, id: id});
+id++;
 }
 grid.push(row);
 }
 return grid;
 }
 
+
+
 //Next, we need to create a function to check for blocks of the same color that are adjacent to each other. We will do this by iterating through the grid and checking each block's neighbors. If we find 3 or more blocks of the same color, we will mark them for destruction.
 
 function checkForMatches(grid) {
-let numRows = grid.length;
-let numColumns = grid[0].length;
-for (let i = 0; i < numRows; i++) {
-for (let j = 0; j < numColumns; j++) {
-let block = grid[i][j];
-// check for matching blocks to the right
-if (j < numColumns - 2 && block.color === grid[i][j + 1].color && block.color === grid[i][j + 2].color) {
-block.destroy = true;
-grid[i][j + 1].destroy = true;
-grid[i][j + 2].destroy = true;
+// check rows
+for (let i = 0; i < grid.length; i++) {
+for (let j = 0; j < grid[0].length; j++) {
+if (grid[i][j]) {
+let color = grid[i][j].color;
+if (j < grid[0].length - 2 && grid[i][j+1] && grid[i][j+2] && grid[i][j+1].color == color && grid[i][j+2].color == color) {
+grid[i][j].destroy = true;
+grid[i][j+1].destroy = true;
+grid[i][j+2].destroy = true;
 }
-// check for matching blocks below
-if (i < numRows - 2 && block.color === grid[i + 1][j].color && block.color === grid[i + 2][j].color) {
-block.destroy = true;
-grid[i + 1][j].destroy = true;
-grid[i + 2][j].destroy = true;
+}
+}
+}
+// check columns
+for (let i = 0; i < grid.length; i++) {
+for (let j = 0; j < grid[0].length; j++) {
+if (grid[i][j]) {
+let color = grid[i][j].color;
+if (i < grid.length - 2 && grid[i+1][j] && grid[i+2][j] && grid[i+1][j].color == color && grid[i+2][j].color == color) {
+grid[i][j].destroy = true;
+grid[i+1][j].destroy = true;
+grid[i+2][j].destroy = true;
+}
 }
 }
 }
@@ -39,52 +55,62 @@ grid[i + 2][j].destroy = true;
 //Now we need to create a function to destroy the marked blocks and move all the blocks down to fill in the empty spaces.
 
 function destroyBlocks(grid) {
-let numRows = grid.length;
-let numColumns = grid[0].length;
-// destroy marked blocks
-for (let i = 0; i < numRows; i++) {
-for (let j = 0; j < numColumns; j++) {
-if (grid[i][j].destroy) {
-grid[i][j] = null;
+// fill in empty spaces with existing blocks
+for (let i = grid.length - 1; i >= 0; i--) {
+for (let j = 0; j < grid[0].length; j++) {
+if (!grid[i][j]) {
+let k = i;
+while (k > 0 && !grid[k][j]) {
+k--;
+}
+grid[i][j] = grid[k][j];
+grid[k][j] = null;
 }
 }
 }
-// move blocks down to fill in empty spaces
-for (let j = 0; j < numColumns; j++) {
-let numEmptySpaces = 0;
-for (let i = numRows - 1; i >= 0; i--) {
-if (grid[i][j] === null) {
-numEmptySpaces++;
-} else if (numEmptySpaces > 0) {
-grid[i + numEmptySpaces][j] = grid[i][j];
-grid[i][j] = null;
-}
-}
-}
-}
-
-//Finally, we can create a function to run the game loop. This function will generate the grid, check for matches, destroy the marked blocks, and repeat until there are no more matches.
-
-function runGame() {
-let grid = generateGrid(10, 10);
-let hasMatches = true;
-while (hasMatches) {
-checkForMatches(grid);
-destroyBlocks(grid);
-hasMatches = false;
+// create new blocks
 for (let i = 0; i < grid.length; i++) {
 for (let j = 0; j < grid[0].length; j++) {
 if (grid[i][j] && grid[i][j].destroy) {
-hasMatches = true;
-break;
-}
-}
-if (hasMatches) {
-break;
+let color = colors[Math.floor(Math.random() * colors.length)];
+grid[i][j] = {color: color, id: id};
+id++;
 }
 }
 }
 }
+
+
+function handleBlockClick(row, column, grid) {
+let table = document.querySelector("table");
+if (!selectedBlock) {
+selectedBlock = {row: row, column: column};
+table.rows[row].cells[column].classList.add("selected");
+} else {
+// attempt to swap blocks
+let temp = grid[selectedBlock.row][selectedBlock.column];
+grid[selectedBlock.row][selectedBlock.column] = grid[row][column];
+grid[row][column] = temp;
+table.rows[selectedBlock.row].cells[selectedBlock.column].classList.remove("selected");
+selectedBlock = null;
+}
+// check for matches and destroy blocks
+checkForMatches(grid);
+destroyBlocks(grid);
+// remove existing table element
+document.body.removeChild(table);
+// render updated grid
+document.body.appendChild(renderGrid(grid));
+}
+
+
+
+
+
+
+
+
+
 
 //To run the game, we just need to call the "runGame" function.
 
@@ -97,24 +123,31 @@ let row = document.createElement("tr");
 for (let j = 0; j < grid[0].length; j++) {
 let cell = document.createElement("td");
 if (grid[i][j]) {
-cell.classList.add(grid[i][j].color);
+cell.classList.add("block");
+cell.style.backgroundColor = grid[i][j].color;
+cell.innerHTML = grid[i][j].id;
 }
+cell.addEventListener("click", function() {
+handleBlockClick(i, j, grid);
+});
 row.appendChild(cell);
 }
 table.appendChild(row);
 }
-document.body.appendChild(table);
+return table;
 }
 
 //We can then call the "renderGrid" function after each iteration of the game loop in the "runGame" function.
 
 function runGame() {
-let grid = generateGrid(10, 10);
+	console.log("runGame");
+let grid = generateGrid(numRows, numColumns);
+document.body.appendChild(renderGrid(grid));
 let hasMatches = true;
 while (hasMatches) {
+hasMatches = false;
 checkForMatches(grid);
 destroyBlocks(grid);
-hasMatches = false;
 for (let i = 0; i < grid.length; i++) {
 for (let j = 0; j < grid[0].length; j++) {
 if (grid[i][j] && grid[i][j].destroy) {
@@ -130,4 +163,6 @@ renderGrid(grid);
 }
 }
 
+//let grid = generateGrid(numRows, numColumns);
+//document.body.appendChild(renderGrid(grid));
 //Now when we run the game, we should see the grid of blocks displayed on the page, with the matching blocks disappearing and the remaining blocks falling down to fill in the empty spaces.
